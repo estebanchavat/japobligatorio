81cb0c0961a520d24671daa7062b0760b340493e
estebanchavat <esteban.chavat@gmail.com>
2021-11-14 15:43:05 -0300

    Entrega7
    Se realiza toda la entrega que se pide como obligatorio

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,6 @@
+
+var sumaT =0;
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -20,8 +23,10 @@ document.addEventListener("DOMContentLoaded", () => {
                     <td>` + articulos[i].name + `</td>
                     <td>` + articulos[i].count + `</td>
                     <td > ` + "UYU" + " " + ` <span class="productprice"> ` + costonormalizado +`  </span></td>
-                    <td ><input type='number' onchange="sumar()" value=1 min="0" </td>
-                    <td id="subTotal${i}" class='precio' > ` + costonormalizado + ` </td></tr>
+                    <td ><input type='number' onchange="sumar()"  value=1 min="0" </td>
+                    <td id="subTotal${i}" class='precio' > ` + costonormalizado + ` </td>
+                    </tr>
+
              </tr><br>
              </table>
                     `
@@ -33,9 +38,11 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 })
 
+
+
 function sumar (){
     let precios = document.getElementsByClassName("productprice")
-    let suma =0;
+    let suma =0; 
     let count = document.getElementsByTagName("input") 
     for (i=0; i< precios.length; i++){
         suma+= parseFloat(precios[i].innerHTML) * count[i].value;
@@ -52,3 +59,68 @@ function cambio(moneda,costo){
     }
     return costo
 }
+
+function porcentaje(value) {
+let totalproducts =  document.getElementById("total").innerHTML
+
+let totalenvio = parseFloat(totalproducts) * value;
+let totalmasenvio = parseFloat(totalproducts) + totalenvio;
+
+document.getElementById("costoenvio").innerHTML = (totalenvio).toFixed(2)
+document.getElementById("totalenvio").innerHTML = (totalmasenvio).toFixed(2)
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+  function tarjetadecredito() {
+    document.getElementById("validateemail").disabled = true;
+    document.getElementById("ccbank").disabled = true;
+    document.getElementById("validatecount").disabled = true;
+    document.getElementById("validatename").disabled = false;
+    document.getElementById("validatenumber").disabled = false;
+    document.getElementById("validatedate").disabled = false;
+    document.getElementById("validatecvv").disabled = false;
+}
+
+function paypal(){
+    document.getElementById("validatename").disabled = true;
+    document.getElementById("validatenumber").disabled = true;
+    document.getElementById("validatedate").disabled = true;
+    document.getElementById("validatecvv").disabled = true;
+    document.getElementById("ccbank").disabled = true;
+    document.getElementById("validatecount").disabled = true;
+    document.getElementById("validateemail").disabled = false;
+
+    
+}
+   
+function bank(){
+    document.getElementById("validatename").disabled = true;
+    document.getElementById("validatenumber").disabled = true;
+    document.getElementById("validatedate").disabled = true;
+    document.getElementById("validatecvv").disabled = true;
+    document.getElementById("validateemail").disabled = true;
+    document.getElementById("validatecount").disabled = false;
+    document.getElementById("ccbank").disabled = false;
+}
+   
+
